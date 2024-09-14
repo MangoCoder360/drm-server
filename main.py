70e0aa63e775d0ace672216ddc19a0dba5345660
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2024-09-14 13:10:23 -0700

    add license keys

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, request
+import random, string, json
 
-AUTH_SECRET = 'OurHardWorkGuardedByTheseWordsPleaseDontSteal'
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return redirect(url_for('admin'))
+@app.route("/api/applications/<string:application>/license-keys/check")
+def license_key_check(application):
+    licenseKey = request.args.get('key')
+    allKeysForApplication = json.loads(open("license-keys.json").read())
+    if application in allKeysForApplication:
+        if licenseKey in allKeysForApplication[application]:
+            return allKeysForApplication[application][licenseKey]
+        else:
+            return {"status": "invalid", "minVersion": 1}
+    else:
+        return {"status": "invalid", "minVersion": 1}
 
-@app.route("/api/application/<string:app_name>")
-
-@app.route("/admin")
-def admin():
-    return render_template('dashboard.html')
+def generate_license_key():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=4)) + "-" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=4)) + "-" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=4)) + "-" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
 
 if __name__ == '__main__':
-    print("Launching DRM Server...")
-    app.run()
+    app.run(host="0.0.0.0", port=5502)
