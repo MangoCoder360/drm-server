bb913090f16bde5001f4a0fb4de0f696c941fb0c
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2024-11-11 21:39:10 -0800

    over-the-air update system

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from flask import Flask, request, redirect, render_template, session
-import random, string, json
+from flask import Flask, request, redirect, render_template, session, send_file, abort
+from functools import wraps
+import random, string, json, time, subprocess
 
 app = Flask(__name__)
 app.secret_key = str(random.randint(0, 1000000000))
+
+# AUTHENTICATION DECORATORS
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not "username" in session or session["login_area"] != "admin":
+            abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
+
+def user_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not "username" in session or session["login_area"] != "user":
+            abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
 
 # UTILITY FUNCTIONS
 def generate_license_key():
@@ -33,6 +51,31 @@ def get_licenses_for_application(application):
         all_keys_for_application = json.load(file)
     return all_keys_for_application.get(application, {})
 
+def get_ota_info_for_application(application):
+    with open("ota.json", "r") as file:
+        ota_info = json.load(file)
+    return ota_info.get(application, {})
+
+def get_ota_info_for_license(application, license_key, parameter):
+    with open("ota.json", "r") as file:
+        ota_info = json.load(file)
+    return ota_info[application]["clientConfigs"][license_key].get(parameter, None)
+
+def update_ota_app_config(application, parameter, value):
+    with open("ota.json", "r") as file:
+        ota_info = json.load(file)
+    ota_info[application][parameter] = value
+    with open("ota.json", "w") as file:
+        json.dump(ota_info, file)
+
+def update_ota_client_config(application, license_key, parameter, value):
+    with open("ota.json", "r") as file:
+        ota_info = json.load(file)
+    ota_info[application]["clientConfigs"][license_key][parameter] = value
+    with open("ota.json", "w") as file:
+        json.dump(ota_info, file)
+    
+
 # API ENDPOINTS
 @app.route("/api/applications/<string:application>/license-keys/check")
 def license_key_check(application):
@@ -45,6 +88,65 @@ def license_key_check(application):
             return {"status": "invalid", "minVersion": 1}
     else:
         return {"status": "invalid", "minVersion": 1}
+    
+@app.route("/api/applications/<string:application>/ota/latest-version")
+def ota_current_version(application):
+    license_key = request.args.get("key")
+    key_file = json.loads(open("license-keys.json").read())
+    if application in key_file:
+        if license_key in key_file[application]:
+            return get_ota_info_for_application(application)["latestVersion"]
+        else:
+            return "403 Forbidden", 403
+    else:
+        return "403 Forbidden", 403
+
+@app.route("/api/applications/<string:application>/ota/url")
+def ota_url(application):
+    license_key = request.args.get("key")
+    key_file = json.loads(open("license-keys.json").read())
+    if application in key_file:
+        if license_key in key_file[application]:
+            return get_ota_info_for_application(application)["url"]
+        else:
+            return "403 Forbidden", 403
+    else:
+        return "403 Forbidden", 403
+    
+@app.route("/api/applications/<string:application>/ota/client-config")
+def ota_client_config(application):
+    license_key = request.args.get("key")
+    installed_version = request.args.get("version")
+    build_number = request.args.get("build")
+
+    if installed_version is None or build_number is None:
+        return "400 Bad Request", 400
+    
+    key_file = json.loads(open("license-keys.json").read())
+    if application in key_file:
+        if license_key in key_file[application]:
+            update_ota_client_config(application, license_key, "currentVersion", installed_version)
+            update_ota_client_config(application, license_key, "buildNumber", build_number)
+            update_ota_client_config(application, license_key, "lastConfigFetchTime", int(time.time()))
+            return get_ota_info_for_application(application)["clientConfigs"][license_key]
+        else:
+            return "403 Forbidden", 403
+    else:
+        return "403 Forbidden", 403
+
+@app.route("/ota/<string:application>/<string:version>/<string:filename>")
+def ota_download(application, version, filename):
+    license_key = request.args.get("key")
+    key_file = json.loads(open("license-keys.json").read())
+    if application in key_file:
+        if license_key in key_file[application]:
+            filepath = "ota/" + application + "-" + version + "-" + filename
+            try:
+                return send_file(filepath)
+            except:
+                return "404 Not Found", 404
+    return "403 Forbidden", 403
+
   
 # UI ENDPOINTS
 @app.route("/")
@@ -101,18 +203,39 @@ def logout():
     return redirect("/")
 
 @app.route("/admin")
+@admin_required
 def admin():
     return render_template("admin-dash.html", applications=enumerate_applications())
 
 @app.route("/admin/<string:application>")
+@admin_required
 def admin_application(application):
-    return render_template("admin-dash-appspecific.html", application=application, licenses=get_licenses_for_application(application))
+    return render_template("admin-dash-appspecific.html", application=application, licenses=get_licenses_for_application(application), current_version=get_ota_info_for_application(application).get("latestVersion", "0.0.0"))
+
+@app.route("/admin/<string:application>/publishing", methods=["POST","GET"])
+@admin_required
+def admin_publishing(application): 
+    if request.method == "POST":
+        version = request.form.get("version")
+        file = request.files["file"]
+        filepath = "ota/" + application + "-" + version.replace(".","-") + "-" + file.filename
+        file.save(filepath)
+        subprocess.run(["pyminify", filepath, "--in-place"])
+        update_ota_app_config(application, "latestVersion", version)
+        update_ota_app_config(application, "url", "/ota/" + application + "/" + version.replace(".","-") + "/" + file.filename)
+        return redirect("/admin/" + application)
+    return render_template("admin-publish-update.html", application=application, current_version=get_ota_info_for_application(application).get("latestVersion", "0.0.0"))
 
 @app.route("/admin/<string:application>/licenses/<string:license_key>")
+@admin_required
 def admin_manage_license(application, license_key):
-    return render_template("admin-manage-license.html")
+    license = get_licenses_for_application(application).get(license_key, {})
+    secondsSinceLastConfigFetch = int(time.time()) - get_ota_info_for_license(application, license_key, "lastConfigFetchTime")
+    buildNumber = get_ota_info_for_license(application, license_key, "buildNumber")
+    return render_template("admin-manage-license.html", license=license, key=license_key, application=application, secondsSinceLastConfigFetch=secondsSinceLastConfigFetch, buildNumber=buildNumber)
 
 @app.route("/user")
+@user_required
 def user():
     return render_template("user-dash.html", licenses=get_licenses_for_user(session["username"]))
 
